33f07e48cc7cf51c36149719713dc125aa59c6c5
nmd1 <rafty132@gmail.com>
2014-11-07 11:14:56 -0500

    First Commit
    The first of many. Planned out how the program will be designed.

---
+++ IB_Project2/src/ib_project2/Stoicalc.java
@@ -0,0 +1,14 @@
+package ib_project2;
+
+public class Stoicalc {
+    public static void main(String[] args){
+        //and so, the STOIC CALC program begins.
+        
+        //Major Clusters:
+        //GUI: handles all of the visual information and displaying things to the user:
+        //String processing: process chemical equations 
+        //Chem bank: contains data for chemical calculations.
+        //Maths: handles the compelx calculations
+        //Graphing: If there is time: graphs for chemical data.
+    }
+}
