0d1f54ad21871c2b5ad799c6c66b34b23e561c3d
nmd1 <rafty132@gmail.com>
2014-11-21 11:13:36 -0500

    Main window
    Started working on the body of the program.
    
    Also the beginning of the "Many Buttons" problem

--- IB_Project2/src/ib_project2/GUI.java
+++ IB_Project2/src/ib_project2/GUI.java
@@ -1,10 +1,7 @@
 package ib_project2;;
 
 import java.awt.*;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
 import javax.swing.*;
 public class GUI {
     public JFrame main;
@@ -14,6 +11,9 @@ public class GUI {
     Container pane = new Container();
     int xc, yc;
     boolean debug = true;
+    
+    public Label titleLabel = new Label();
+    public Button[] buttons = new Button[100];
     
     public void GUI(){
         main = new JFrame();
@@ -35,28 +35,40 @@ public class GUI {
         
         if(debug == true) main.addMouseListener(new PanelListener());
         
+        mainWindow();
+    }
+    //=====================================BODY==========================================//
+    
+    public void mainWindow() {
+        titleLabel.setText("StoicCalc \n An exploration");
+        Font f = new Font("Bookman Old Style", Font.PLAIN, 30);
+        titleLabel.setFont(f);
+        Layout(titleLabel, 88, 25);
+        titleLabel.setVisible(true);
+        
+        //SOLVE MANY BUTTONS PROBLEM
+        Button conv = new Button();
+        conv = buttons[1];
+        
+        
+        main.add(titleLabel);
+        pack(main);
     }
     
     
+    //===============================HELPER METHODS=====================================//
+    private void pack(JFrame J){
+        J.pack();
+        main.setSize(565,402);
+    }
     
-    
-    
-    
-    //=============DEBUG METHODS========================================================//
-    private class PanelListener extends MouseAdapter { //mouse position
-        @Override
-            public void mousePressed(MouseEvent e) {
-                xc = e.getX();
-                yc = e.getY();
-                System.out.println("(" + (xc - 8) + ", " + (yc - 30) + ")");
-            }
-        }
-    public void Layout(Component c, int x, int y) {
+    private void Layout(Component c, int x, int y) {
         layout.putConstraint(SpringLayout.WEST, c,x, SpringLayout.WEST, pane);
         layout.putConstraint(SpringLayout.NORTH, c,y,SpringLayout.NORTH, pane);
     }
     
-    public void newPanel(JFrame j) {
+    private void newPanel(JFrame j) {
+        
         j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         j.setLocationRelativeTo(null);
         j.setResizable(true);
@@ -67,42 +79,54 @@ public class GUI {
             j.setVisible(false);
         }*/
     }
+    //=================================DEBUG METHODS====================================//
+    private class PanelListener extends MouseAdapter { //mouse position
+        @Override
+            public void mousePressed(MouseEvent e) {
+                if(debug){
+                    xc = e.getX();
+                    yc = e.getY();
+                    System.out.println("(" + (xc - 8) + ", " + (yc - 30) + ")");
+                }
+            }
+    }
     
     public void DebugSize(final JFrame j) {
-        pane.add(debugLabel2);
-        
-        Layout(debugLabel2, 350, 335);
-        pane.add(debugLabel);
-        Layout(debugLabel, 30, 335);
-        main.pack();
-        //If you add components to the frame after it is visible then
-        //you need to revalidate() the JPanel that you add the components to. 
-        j.addComponentListener(new ComponentListener() {
-            
-            @Override
-            public void componentResized(ComponentEvent e) {
-                //System.out.println(j.getSize());
-                Dimension d = j.getSize();
+        if(debug) {
+            pane.add(debugLabel2);
+            Layout(debugLabel2, 350, 335);
+            pane.add(debugLabel);
+            Layout(debugLabel, 30, 335);
+            pack(main);
+            //If you add components to the frame after it is visible then
+            //you need to revalidate() the JPanel that you add the components to. 
+            j.addComponentListener(new ComponentListener() {
 
-                debugLabel.setText(d.toString());
-            }
+                @Override
+                public void componentResized(ComponentEvent e) {
+                    //System.out.println(j.getSize());
+                    Dimension d = j.getSize();
 
-            @Override
-            public void componentMoved(ComponentEvent e) {
-                Point d = j.getLocationOnScreen();
+                    debugLabel.setText(d.toString());
+                }
 
-                debugLabel2.setText(d.toString());
-            }
+                @Override
+                public void componentMoved(ComponentEvent e) {
+                    Point d = j.getLocationOnScreen();
 
-            @Override
-            public void componentShown(ComponentEvent e) {
-                
-            }
+                    debugLabel2.setText(d.toString());
+                }
 
-            @Override
-            public void componentHidden(ComponentEvent e) {
- 
-            }
-        });
+                @Override
+                public void componentShown(ComponentEvent e) {
+
+                }
+
+                @Override
+                public void componentHidden(ComponentEvent e) {
+
+                }
+            });
+        }
     }
 }
